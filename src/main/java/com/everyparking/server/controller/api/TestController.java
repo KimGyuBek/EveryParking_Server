cbcea9b56e9f8a6f0859daab3f60ddb435b9db4e
rlarbqor00 <rlarbqor00@naver.com>
2023-04-06 11:41:40 +0900

    jar commit

--- src/main/java/com/everyparking/server/controller/api/TestController.java
+++ src/main/java/com/everyparking/server/controller/api/TestController.java
@@ -2,8 +2,9 @@ package com.everyparking.server.controller.api;
 
 import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 public class TestController {
 
     @GetMapping("/test")
